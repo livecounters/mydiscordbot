82decf75a687f2f284466ae9b91d2b051f2ff7f2
nazmul2666 <60926653+nazmul2666@users.noreply.github.com>
2020-05-05 08:30:43 +0530

    changed node_modules

--- commands/kick.js
+++ commands/kick.js
@@ -1,18 +1,19 @@
 module.exports = message => {
   const member = message.mentions.members.first();
+  let args = message.content.substring(process.env.BOT_PREFIX).split(" ");
 
   if (!member) {
     return message.reply(
-      `Who are you trying to kick? You must mention a user.`
+      `❌ | Who are you trying to kick? You must mention a user.`
     );
   }
 
   if (!member.kickable) {
-    return message.reply(`I can't kick this user. Sorry!`);
+    return message.reply(`❌ | I can't kick this user. Sorry!`);
   }
 
   return member
     .kick()
     .then(() => message.reply(`${member.user.tag} was kicked.`))
-    .catch(error => message.reply(`Sorry, an error occured.`));
+    .catch(error => message.reply(`❌ | Sorry, an error occured.`));
 };
